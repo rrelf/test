7c4142081eab14aaf4499f843ea6ad65b47f76a0
rrelf <rabenzie1@gmail.com>
2013-06-10 14:57:21 -0500

    adding all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirsApplication::Application.config.secret_token = 'c79518349c2e1313a2c19279c1dbda701f2549861fa1b7c2e01bf776420ca151f77257b3c874ae25720b189166cf09c8a7094f2b2beb96a7682ed1f10a28fec8'
